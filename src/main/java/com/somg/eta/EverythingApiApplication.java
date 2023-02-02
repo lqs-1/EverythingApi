ea6e46f06cd9772e1c4cac9608f490a248a10714
lqs-1 <749062870@qq.com>
2023-02-02 18:01:16 +0800

    创建README文件

---
+++ src/main/java/com/somg/eta/EverythingApiApplication.java
@@ -0,0 +1,13 @@
+package com.somg.eta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverythingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverythingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/somg/eta/EverythingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somg.eta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverythingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
